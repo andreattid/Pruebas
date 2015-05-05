4ce2d3132f63a289110973713fc40a9c534f0af8
andreattid <andreamallo.tid@gmail.com>
2015-05-05 10:03:14 +0200

    iotmine.py

--- iotmine.py
+++ iotmine.py
@@ -1,39 +1,86 @@
+
 from lettuce import *
+from nose.tools import eq_, assert_in, assert_not_equal
+import urllib
+import urllib2
+import json
 
-@step(u'a device with device_id "([^"]*)"')
-def check_dev_id(step, flow_id):
-    world.flow_id = flow_id
 
-@step(u'data "([^"]*)"')
-def check_data(step, flow_data):
-    world.flow_data = flow_data.replace('_', '|')
 
-@step(u'And api_key for service "([^"]*)"')
-def check_apikey(step, flow_apikey):
-    world.flow_apikey = flow_apikey
+@step(u'Given api_key for service "([^"]*)"')
+def given_service(step, service_apikey):
+    world.apikey = service_apikey
 
-@step(u'a request is sent')
-def sending_req(step):
+@step(u'And the service "([^"]*)" has been created')
+def create_service(step, group1):
+    """
+    parsed_url = urlopen('http://10.95.83.239:8080/iot/services ')
+    response = parsed_url.read()
+    resp = json.loads(str(response))
+
+    # Apikey = world.apikey
+    apky = resp["services"]["apikey"]
+    eq_(apky, world.apikey)
+    """
+
+
+@step(u'And a device with device_id "([^"]*)"')
+def given_device_id(step, device_id):
+    world.id = device_id
+
+
+@step(u'And the device with device_id "([^"]*)" has been created')
+def create_device_id(step, group1):
+    """
+    headers = {'Fiware-Service': 'service2', 'Fiware-Subservice': '/srvpath2'}
+    data = urllib.urlencode(headers)
+    req = urllib2.Request('http://10.95.83.239:8080/iot/devices', data)
+    """
+    headers_cb = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    data_cb = urllib.urlencode(headers_cb)
+    req_cb = urllib2.Request('http://10.95.82.244:1026/v1/updateContext', None, headers_cb)
+
+    response = urllib2.urlopen(req_cb)
+    page = response.read()
+    resp = json.loads(str(page))
+
+    print data_cb
+
+    # Apikey = world.apikey
+    """count = 0
+    size = resp["count"]
+
+    for devices in resp["devices"][count]["device_id"]:
+        assert devices
+    """
+
+
+@step(u'And data "([^"]*)"')
+def given_data(step, data):
+    world.data = data
+
+
+##
+
+@step(u'When a measure is received')
+def receive_measure(step):
     assert True
 
-@step(u'the IoT response should be ')
-def iot_resp(step):
+@step(u'Then the IoT response should be 200')
+def iot_response(step):
     assert True
 
-@step(u'And the response sent to Context Broker should include id "([^"]*)" and type "([^"]*)"')
-def body_shape1(step, iot_id, iot_type):
-    assert world.flow_id == iot_id
-    print(world.flow_id)
-    print(iot_id)
-    print step.hashes[0]
+@step(u'And context broker is notified')
+def cb_notification(step):
+    assert True
 
-@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)"')
-def body_shape2(step, iot_name, iot_val):
-    name, val = world.flow_data.replace('|', ' ').split()
-    assert name == iot_name
-    print step.hashes[0]
-
-
-
-
-
+@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
+def cb_response(step, name, value, dev_id):
+    aux_name, aux_val = world.data.replace('_', ' ').split()
+    # This will change as the literal in UL2.0 is given by 'key|value'
+    assert True
+    """
+    eq_(aux_name, name)
+    eq_(aux_val, value)
+    eq_(world.id, dev_id)
+   """
--- mine.feature
+++ mine.feature
@@ -1,23 +1,32 @@
 Feature: Receive measures from sensor
 	In simplest scenario, sending with GET will only collect one measure
 
-Scenario Outline: Send data with GET to new device
-	Given a device with device_id "<ID>"
+Scenario Outline: Device send a new measure
+	Given api_key for service "<SERVICE>"
+    And the service "<SERVICE>" has been created
+    And a device with device_id "<ID>"
+    And the device with device_id "<ID>" has been created
 	And data "<DATA>"
-	And api_key for service "<SERVICE>"
-	When a request is sent
+	When a measure is received
 	Then the IoT response should be 200
-	And the response sent to Context Broker should include id "<IOT_RESP_ID>" and type "<IOT_RESP_TYPE>"
-								|id            |type    |
-								|thing:001     |thing   |
-    And the response sent to Context Broker should include name "<IOT_RESP_NAME>" and value "<IOT_RESP_VAL>"
-								|name          |value    |
-								|t             |15       |
-	
+	And context broker is notified
+    And the response sent to Context Broker should include name "<IOT_RESP_NAME>" and value "<IOT_RESP_VAL>" and id "<IOT_RESP_ID>"
+                                |name          |value     |id            |
+								|t             |15        |flow0         |
+                                |h             |771       |flow1         |
+
 
 Examples:
-    | ID       | DATA    | SERVICE  | IOT_RESP_ID    | IOT_RESP_TYPE    | IOT_RESP_NAME | IOT_RESP_VAL  | STATUS
-    | FLOW0    | t_15    | service1 | FLOW0          | temperature      | t             | 15            | UPDATED
-    | FLOW1    | h_771   | service1 | FLOW1          | humidity         | h             | 771           | UPDATED
+    | ID          | DATA    | SERVICE  |
+    | DEVICE_1    | t_15    | service1 |
+    | DEVICE_2    | h_771   | service1 |
+
+	# Using | to separate data is not possible --> _ instead
+	
+
+
 	
 	
+	
+	
+ 

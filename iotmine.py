a6dee56162d51fe82cf4f578057f7590bbdf5caf
andreattid <andreamallo.tid@gmail.com>
2015-05-05 14:09:28 +0200

    iotmine.py

--- iotmine.py
+++ iotmine.py
@@ -1,9 +1,26 @@
-
 from lettuce import *
-from nose.tools import eq_, assert_in, assert_not_equal
+from nose.tools import eq_
 import urllib
 import urllib2
+import requests
 import json
+
+
+@step(u'Given a test')
+def given_test(step):
+    url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/device_id1'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    req = requests.get(url=url, headers=headers, data=None)
+    text = req.text
+    resp = json.loads(str(text))
+
+    for attributes in resp["contextElement"]["attributes"]:
+
+        print attributes
+
+
+
+
 
 
 
@@ -13,74 +30,74 @@ def given_service(step, service_apikey):
 
 @step(u'And the service "([^"]*)" has been created')
 def create_service(step, group1):
-    """
-    parsed_url = urlopen('http://10.95.83.239:8080/iot/services ')
-    response = parsed_url.read()
-    resp = json.loads(str(response))
+    # Coger el GET que devuelve los servicios y buscar en el json el world.apikey
 
-    # Apikey = world.apikey
+    url = 'http://10.95.83.239:8080/iot/services'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    req = requests.get(url=url, headers=headers, data=None)
+    text = req.text
+    resp = json.loads(str(text))
     apky = resp["services"]["apikey"]
     eq_(apky, world.apikey)
-    """
-
 
 @step(u'And a device with device_id "([^"]*)"')
 def given_device_id(step, device_id):
     world.id = device_id
 
-
 @step(u'And the device with device_id "([^"]*)" has been created')
 def create_device_id(step, group1):
-    """
+    # Buscar en el JSON si se ha creado un device similar al solicitado
+
+    url = 'http://10.95.83.239:8080/iot/devices'
     headers = {'Fiware-Service': 'service2', 'Fiware-Subservice': '/srvpath2'}
-    data = urllib.urlencode(headers)
-    req = urllib2.Request('http://10.95.83.239:8080/iot/devices', data)
-    """
-    headers_cb = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
-    data_cb = urllib.urlencode(headers_cb)
-    req_cb = urllib2.Request('http://10.95.82.244:1026/v1/updateContext', None, headers_cb)
+    req = requests.get(url=url, headers=headers, data=None)
+    resp = json.loads(str(req))
 
-    response = urllib2.urlopen(req_cb)
-    page = response.read()
-    resp = json.loads(str(page))
-
-    print data_cb
-
-    # Apikey = world.apikey
-    """count = 0
+    count = 0
     size = resp["count"]
 
-    for devices in resp["devices"][count]["device_id"]:
-        assert devices
-    """
+    # Esto lleva un loop porque los devices devueltos van en un array, y necesito contabilizarlos para recorrerlo
+    ##HAY QUE ACABARLO PORQUE NO SE HACERLO SIN SEGUIR PROBANDO
+    for device in resp["devices"][count]["device_id"]:
+        if device != world.id:
+            count = count + 1
+
+
+
+
+
 
 
 @step(u'And data "([^"]*)"')
 def given_data(step, data):
     world.data = data
 
-
-##
-
 @step(u'When a measure is received')
 def receive_measure(step):
+    # Comprobar que la medida se lanza desde dispo - GET y POST options
     assert True
+
 
 @step(u'Then the IoT response should be 200')
 def iot_response(step):
     assert True
 
+
 @step(u'And context broker is notified')
 def cb_notification(step):
     assert True
 
+
 @step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
 def cb_response(step, name, value, dev_id):
-    aux_name, aux_val = world.data.replace('_', ' ').split()
-    # This will change as the literal in UL2.0 is given by 'key|value'
-    assert True
-    """
+
+    url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/device_id1'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    req = requests.get(url=url, headers=headers, data=None)
+    text = req.text
+    resp = json.loads(str(text))
+
+    aux_name = context
     eq_(aux_name, name)
     eq_(aux_val, value)
     eq_(world.id, dev_id)
-   """
--- mine.feature
+++ mine.feature
@@ -1,28 +1,43 @@
 Feature: Receive measures from sensor
-	In simplest scenario, sending with GET will only collect one measure
+  In simplest scenario, sending with GET will only collect one measure
 
-Scenario Outline: Device send a new measure
-	Given api_key for service "<SERVICE>"
+  @1
+  Scenario Outline: Device send a new measure
+    Given api_key for service "<SERVICE>"
     And the service "<SERVICE>" has been created
     And a device with device_id "<ID>"
     And the device with device_id "<ID>" has been created
-	And data "<DATA>"
-	When a measure is received
-	Then the IoT response should be 200
-	And context broker is notified
+    And data "<DATA>"
+    When a measure is received
+    Then the IoT response should be 200
+    And context broker is notified
     And the response sent to Context Broker should include name "<IOT_RESP_NAME>" and value "<IOT_RESP_VAL>" and id "<IOT_RESP_ID>"
-                                |name          |value     |id            |
-								|t             |15        |flow0         |
-                                |h             |771       |flow1         |
+      | name | value | id    |
+      | t    | 15    | flow0 |
+      | h    | 771   | flow1 |
 
 
-Examples:
-    | ID          | DATA    | SERVICE  |
-    | DEVICE_1    | t_15    | service1 |
-    | DEVICE_2    | h_771   | service1 |
+  Examples:
+    | ID       | DATA  | SERVICE  |
+    | DEVICE_1 | t_15  | service1 |
+    | DEVICE_2 | h_771 | service1 |
 
-	# Using | to separate data is not possible --> _ instead
-	
+ # Using | to separate data is not possible --> _ instead
+
+
+
+
+
+
+  @2
+  Scenario Outline: Device send a new measure
+    Given a test
+
+
+  Examples:
+    | ID       | DATA  | SERVICE  |
+    | DEVICE_1 | t_15  | service1 |
+
 
 
 	

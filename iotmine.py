fa7b01de725b736ce6a956bfce6be44e38881e3d
andreattid <andreamallo.tid@gmail.com>
2015-05-06 12:09:13 +0200

    iotmine.py

--- iotmine.py
+++ iotmine.py
@@ -8,11 +8,10 @@ import json
 
 @step(u'Given a test')
 def given_test(step):
+    name = 'temperature'
+    value = '17'
+    print world.resp
 
-    url = 'http://192.168.22.137:8085/iot/d?i=device_id&d=att_name|15&k=apikey3'
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
-    req = requests.get(url=url, headers=headers, data=None)
-    eq_(req.status_code,200)
 
 
 
@@ -22,7 +21,7 @@ def given_test(step):
 
 @step(u'Given api_key for service "([^"]*)"')
 def given_service(step, service_apikey):
-    world.apikey = service_apikey
+    world.apikey = 'apikey3'
 
 @step(u'And the service "([^"]*)" has been created')
 def create_service(step, group1):
@@ -40,11 +39,10 @@ def create_service(step, group1):
 
 @step(u'And a device with device_id "([^"]*)"')
 def given_device_id(step, device_id):
-    world.id = device_id
+    world.id = 'device_id1'
 
 @step(u'And the device with device_id "([^"]*)" has been created')
 def create_device_id(step, group1):
-
     url = 'http://192.168.22.137:8085/iot/devices'
     headers = {'Fiware-Service': 'service2', 'Fiware-Subservice': '/srvpath2'}
     req = requests.get(url=url, headers=headers, data=None)
@@ -85,9 +83,12 @@ def cb_notification(step):
 
 @step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
 def cb_response(step, name, value, dev_id):
+    name1 = 'temperature'
+    value1 = '17'
+    
     attributes = world.resp["contextElement"]["attributes"]
 
     for attribute in attributes:
         if attribute["name"] == name:
-            eq_(attribute["name"], name)
-            eq_(attribute["value"],value)
+            eq_(attribute["name"], name1)
+            eq_(attribute["value"],value1)

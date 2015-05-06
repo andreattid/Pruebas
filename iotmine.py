859cae8b3c320c8318693b9d3bda5fa44955db9b
andreattid <andreamallo.tid@gmail.com>
2015-05-06 13:22:54 +0200

    iotmine.py

--- iotmine.py
+++ iotmine.py
@@ -6,22 +6,36 @@ import requests
 import json
 
 
+"""******************************************************
+
+
+        SET OF STEPS IN SCENARIO OUTLINE @3
+                  TESTING SCENARIO
+
+
+*******************************************************"""
+
 @step(u'Given a test')
 def given_test(step):
-    name = 'temperature'
-    value = '17'
-    print world.resp
+
+    data = 'temperature_17#humidity_711'
+    print data.replace('_','|')
 
 
 
 
 
+"""******************************************************
 
 
+        SET OF STEPS IN SCENARIO OUTLINE @1
+
+
+*******************************************************"""
 
 @step(u'Given api_key for service "([^"]*)"')
 def given_service(step, service_apikey):
-    world.apikey = 'apikey3'
+    world.apikey = service_apikey
 
 @step(u'And the service "([^"]*)" has been created')
 def create_service(step, group1):
@@ -37,9 +51,11 @@ def create_service(step, group1):
         eq_(service["apikey"], world.apikey, 'El servicio solicitado si existe')
         return
 
+
 @step(u'And a device with device_id "([^"]*)"')
 def given_device_id(step, device_id):
-    world.id = 'device_id1'
+    world.id = device_id
+
 
 @step(u'And the device with device_id "([^"]*)" has been created')
 def create_device_id(step, group1):
@@ -54,25 +70,29 @@ def create_device_id(step, group1):
         eq_(device["device_id"], world.id)
         return
 
+
 @step(u'And data "([^"]*)"')
 def given_data(step, data):
-    world.data = data
+    world.data = data.replace('_', '|')
+
 
 @step(u'When a measure is received')
 def receive_measure(step):
     # IDEA?
     assert True
 
-@step(u'Then the IoT response should be 200')
-def iot_response(step):
-    url = 'http://192.168.22.137:8085/iot/d?i=device_id&d=att_name|15&k=apikey3'
+
+@step(u'Then the IoT GET response should be 200')
+def iot_get_response(step):
+    url = 'http://192.168.22.137:8085/iot/d?i='+world.id+'&d='+world.data+'&k='+world.apikey
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
     req = requests.get(url=url, headers=headers, data=None)
     eq_(req.status_code,200)
 
+
 @step(u'And context broker is notified')
 def cb_notification(step):
-    url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/device_id1'
+    url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/'+world.id
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
     req = requests.get(url=url, headers=headers, data=None)
     status = req.status_code
@@ -81,14 +101,44 @@ def cb_notification(step):
     text = req.text
     world.resp = json.loads(str(text))
 
-@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
-def cb_response(step, name, value, dev_id):
+
+@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)"')
+def cb_response(step, name, value):
     name1 = 'temperature'
     value1 = '17'
-    
+
     attributes = world.resp["contextElement"]["attributes"]
 
     for attribute in attributes:
-        if attribute["name"] == name:
+        if attribute["name"] == name1:
             eq_(attribute["name"], name1)
-            eq_(attribute["value"],value1)
+            eq_(attribute["value"], value1)
+
+
+
+
+"""******************************************************
+
+
+         STEPS ADDED TO SCENARIO OUTLINE @2
+
+
+*******************************************************"""
+@step(u'And several measures in data "([^"]*)"')
+def and_several_measures_in_data_group1(step, data):
+    world.multidata = data.replace('_', '|')
+
+@step(u'Then the IoT POST response should be 200')
+def iot_post_response(step):
+    url = 'http://192.168.22.137:8085/iot/d?i='+world.id+'&k='+world.apikey
+    headers = {'Content-Type': 'application/json'}
+    data = world.multidata
+    req = requests.post(url=url, headers=headers, data=data)
+    eq_(req.status_code, 200)
+
+
+
+
+
+
+

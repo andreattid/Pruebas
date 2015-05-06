ddfd345e9e31c42b2d4d540013b20061245786b0
andreattid <andreamallo.tid@gmail.com>
2015-05-06 11:55:17 +0200

    iotmine.py

--- iotmine.py
+++ iotmine.py
@@ -1,5 +1,5 @@
 from lettuce import *
-from nose.tools import eq_
+from nose.tools import eq_, assert_in
 import urllib
 import urllib2
 import requests
@@ -8,15 +8,11 @@ import json
 
 @step(u'Given a test')
 def given_test(step):
-    url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/device_id1'
+
+    url = 'http://192.168.22.137:8085/iot/d?i=device_id&d=att_name|15&k=apikey3'
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
     req = requests.get(url=url, headers=headers, data=None)
-    text = req.text
-    resp = json.loads(str(text))
-
-    for attributes in resp["contextElement"]["attributes"]:
-
-        print attributes
+    eq_(req.status_code,200)
 
 
 
@@ -30,15 +26,17 @@ def given_service(step, service_apikey):
 
 @step(u'And the service "([^"]*)" has been created')
 def create_service(step, group1):
-    # Coger el GET que devuelve los servicios y buscar en el json el world.apikey
 
-    url = 'http://10.95.83.239:8080/iot/services'
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    url = 'http://192.168.22.137:8085/iot/services'
+    headers = {'Fiware-Service': 'service2', 'Fiware-Subservice': '/srvpath2'}
     req = requests.get(url=url, headers=headers, data=None)
     text = req.text
     resp = json.loads(str(text))
-    apky = resp["services"]["apikey"]
-    eq_(apky, world.apikey)
+    services = resp["services"]
+
+    for service in services:
+        eq_(service["apikey"], world.apikey, 'El servicio solicitado si existe')
+        return
 
 @step(u'And a device with device_id "([^"]*)"')
 def given_device_id(step, device_id):
@@ -46,27 +44,17 @@ def given_device_id(step, device_id):
 
 @step(u'And the device with device_id "([^"]*)" has been created')
 def create_device_id(step, group1):
-    # Buscar en el JSON si se ha creado un device similar al solicitado
 
-    url = 'http://10.95.83.239:8080/iot/devices'
+    url = 'http://192.168.22.137:8085/iot/devices'
     headers = {'Fiware-Service': 'service2', 'Fiware-Subservice': '/srvpath2'}
     req = requests.get(url=url, headers=headers, data=None)
-    resp = json.loads(str(req))
+    text = req.text
+    resp = json.loads(str(text))
+    devices = resp["devices"]
 
-    count = 0
-    size = resp["count"]
-
-    # Esto lleva un loop porque los devices devueltos van en un array, y necesito contabilizarlos para recorrerlo
-    ##HAY QUE ACABARLO PORQUE NO SE HACERLO SIN SEGUIR PROBANDO
-    for device in resp["devices"][count]["device_id"]:
-        if device != world.id:
-            count = count + 1
-
-
-
-
-
-
+    for device in devices:
+        eq_(device["device_id"], world.id)
+        return
 
 @step(u'And data "([^"]*)"')
 def given_data(step, data):
@@ -74,30 +62,32 @@ def given_data(step, data):
 
 @step(u'When a measure is received')
 def receive_measure(step):
-    # Comprobar que la medida se lanza desde dispo - GET y POST options
+    # IDEA?
     assert True
-
 
 @step(u'Then the IoT response should be 200')
 def iot_response(step):
-    assert True
-
+    url = 'http://192.168.22.137:8085/iot/d?i=device_id&d=att_name|15&k=apikey3'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
+    req = requests.get(url=url, headers=headers, data=None)
+    eq_(req.status_code,200)
 
 @step(u'And context broker is notified')
 def cb_notification(step):
-    assert True
-
-
-@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
-def cb_response(step, name, value, dev_id):
-
     url = 'http://10.95.82.244:1026/v1/contextEntities/type/thing/id/device_id1'
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Fiware-Service': 'service2'}
     req = requests.get(url=url, headers=headers, data=None)
-    text = req.text
-    resp = json.loads(str(text))
+    status = req.status_code
+    eq_(status, 200)
 
-    aux_name = context
-    eq_(aux_name, name)
-    eq_(aux_val, value)
-    eq_(world.id, dev_id)
+    text = req.text
+    world.resp = json.loads(str(text))
+
+@step(u'And the response sent to Context Broker should include name "([^"]*)" and value "([^"]*)" and id "([^"]*)"')
+def cb_response(step, name, value, dev_id):
+    attributes = world.resp["contextElement"]["attributes"]
+
+    for attribute in attributes:
+        if attribute["name"] == name:
+            eq_(attribute["name"], name)
+            eq_(attribute["value"],value)
